7e1112283bf84d8499d3214442aec4811500f362
drana <dipen.rana@hotmail.com>
2016-05-29 10:55:49 -0700

    removed softinput

--- app/src/main/java/dipen/todoapp/EditItemActivity.java
+++ app/src/main/java/dipen/todoapp/EditItemActivity.java
@@ -1,10 +1,12 @@
 package dipen.todoapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
--- app/src/main/java/dipen/todoapp/MainActivity.java
+++ app/src/main/java/dipen/todoapp/MainActivity.java
@@ -1,6 +1,5 @@
 package dipen.todoapp;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
