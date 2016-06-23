576b4f15e24339eeed5215552040490dcb34db96
drana <dipen.rana@hotmail.com>
2016-06-22 19:54:48 -0700

    custom Adapter

--- app/src/main/java/dipen/todoapp/MainActivity.java
+++ app/src/main/java/dipen/todoapp/MainActivity.java
@@ -1,28 +1,41 @@
 package dipen.todoapp;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     Button btnaddNewItem;
     Button btnBlank;
     EditText etNewItem;
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
+/*    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;*/
+    ArrayList<TodoItem> arrayofItems = new ArrayList<TodoItem>();
+    ItemsAdapter itemsAdapter;
     ListView lvItems;
     private final int REQUEST_CODE = 20;
     private int textPosition = 0;
@@ -33,16 +46,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnaddNewItem = (Button) findViewById(R.id.btn_AddItem);
-        btnBlank = (Button) findViewById(R.id.button_Blank);
+        //btnBlank = (Button) findViewById(R.id.button_Blank);
         etNewItem = (EditText) findViewById(R.id.et_NewItem);
         lvItems = (ListView) findViewById(R.id.lv_ListofItems);
         //check for existing items and read them
-        readItems();
+        //check for existing items and read them
+        try {
+            readItems();}
+        catch(ClassNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
         //attach adapter the listview
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        itemsAdapter  = new ItemsAdapter(this,arrayofItems );
         lvItems.setAdapter(itemsAdapter);
 
         //click listener's
+        etNewItem.addTextChangedListener(newItemTextWatcher);
         setupButtonOnClickListener();
         setupListViewListener();
     }
@@ -54,7 +75,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 textPosition = position;
-                launchEditItemActivity(items.get(position).toString());
+                //launchEditItemActivity(items.get(position).toString());
+                onShowEditItem(arrayofItems.get(position).toString());
             }
         });
 
@@ -63,12 +85,19 @@ public class MainActivity extends AppCompatActivity {
                 new AdapterView.OnItemLongClickListener() {
                     @Override
                     public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
-                        items.remove(position);
+                        arrayofItems.remove(position);
                         itemsAdapter.notifyDataSetChanged();
                         writeItems();
                         return true;
                     }
                 });
+    }
+
+    private void onShowEditItem(String editedItem) {
+        FragmentManager fm = getSupportFragmentManager();
+        EditItemFragment editItemFragment = EditItemFragment.newInstance("Some Title");
+        editItemFragment.show(fm,"fragment_edit_item");
+
     }
 
     private void launchEditItemActivity(String editItemText) {
@@ -84,8 +113,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void updateEditedText(String temp) {
-        items.set(textPosition,temp);
+    private void updateEditedText(String updatedString) {
+        //arrayofItems.set(textPosition,updatedString);
         itemsAdapter.notifyDataSetChanged();
         writeItems();
     }
@@ -99,9 +128,6 @@ public class MainActivity extends AppCompatActivity {
                 switch (v.getId()){
                     case R.id.btn_AddItem:
                         OnAddNewItem(v);
-                        break;
-                    case R.id.button_Blank:
-                        OnBlankBtnPress(v);
                         break;
                     default:
                         break;
@@ -119,33 +145,77 @@ public class MainActivity extends AppCompatActivity {
 
     private void OnAddNewItem(View view) {
         etNewItem = (EditText) findViewById(R.id.et_NewItem);
-        String newItemText = etNewItem.getText().toString();
-        itemsAdapter.add(newItemText);
+        String newItemText = "";
+        if(etNewItem != null) {
+            newItemText = etNewItem.getText().toString();
+        }
+        if(newItemText.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), "",
+                    Toast.LENGTH_SHORT).show();
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
+        Calendar c = Calendar.getInstance();
+        String currentDate = sdf.format(new Date());
+        TodoItem newItem = new TodoItem(newItemText,currentDate);
+        itemsAdapter.add(newItem);
         etNewItem.setText("");
         writeItems();
     }
 
-    private void readItems(){
-        File fileDir = getFilesDir();
-        File todoFile = new File(fileDir,"todo.txt");
-        try{
-                items = new ArrayList<String>(FileUtils.readLines(todoFile));
+    private void readItems() throws IOException, ClassNotFoundException{
+        try {
+            String filename = "todo.srl";
+            ObjectInputStream input = new ObjectInputStream(new FileInputStream(new File(new File(getFilesDir(),"")+File.separator+filename)));
 
-        }catch (IOException e){
-            items = new ArrayList<String>();
-        }
-    }
+            //ArrayList<TodoItem> itemsObject = (ArrayList<TodoItem>) input.readObject();
+            arrayofItems = (ArrayList<TodoItem>) input.readObject();
+            input.close();
 
-    private void writeItems(){
-        File fileDir = getFilesDir();
-        File todoFile = new File(fileDir, "todo.txt");
-        try{
-            FileUtils.writeLines(todoFile,items);
-
-        }catch (IOException e){
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
 
+    private void writeItems() {
+        String filename = "todo.srl";
+        try {
+            FileOutputStream fos = new FileOutputStream(new File(getFilesDir(), "") + File.separator + filename);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(arrayofItems);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-}
+    //  create a textWatcher member to enable and disable AddNewItem button with respect to etNewItem
+    private TextWatcher newItemTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+            // check etNewItem for empty values
+            checkNewItemForEmptyValues();
+        }
+        void checkNewItemForEmptyValues(){
+            Button btnAddItem = (Button)findViewById(R.id.btn_AddItem);
+
+            String newItem = etNewItem.getText().toString();
+
+            if(newItem.equals("")){
+                btnAddItem.setEnabled(false);
+            } else {
+                btnAddItem.setEnabled(true);
+            }
+        }
+    };
+    }
+
